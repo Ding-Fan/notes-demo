91509c3081b9a9feff16f72bfe00c36aa96b25c4
Fan Ding <zichuange@gmail.com>
2020-02-28 01:38:16 +0800

    ➕ Add react-native-paper

---
+++ screens/ViewNotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const ViewNotes = () => {
+  return (
+    <View>
+      <Text>Text </Text>
+    </View>
+  );
+};
+
+export default ViewNotes;
+
+const styles = StyleSheet.create({});
